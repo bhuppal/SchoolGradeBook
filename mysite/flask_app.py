891f35aa3849feddda379cdafb1c476e9a942b94
Bhuppal Kumar <bhuppal@hotmail.com>
2019-04-23 11:06:08 +0000

    Homepage template

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -2,6 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Flask!'
 
+
+@app.route('/hello/John')
+def john():
+    return render_template('hello.html', name = 'John')
