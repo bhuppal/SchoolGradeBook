ce575ff06491fe0e29f6255aa97a36f6de806317
Bhuppal Kumar <bhuppal@hotmail.com>
2019-05-07 19:55:27 +0000

    Added virtualenv folder

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,19 +1,109 @@
 
 # A very simple Flask Hello World app for you to get started with...
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash
 
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+app.config["DEBUG"] = True
+app.config['TESTING'] = False
 
-@app.route("/login/")
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="cindy2bhuppal",
+    password="school123",
+    hostname="cindy2bhuppal.mysql.pythonanywhere-services.com",
+    databasename="cindy2bhuppal$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+app.secret_key = "Saibaba"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(username=user_id).first()
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now)
+    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    commenter = db.relationship('User', foreign_keys=commenter_id)
+
+#comments = []
+
+@app.route("/login/", methods=["GET", "POST"])
 def login():
-    return render_template("login_page.html")
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
 
-@app.route('/hello/John')
-def john():
-    return render_template('hello.html', name = 'John')
+    user = load_user(request.form["username"])
+    if user is None:
+        return render_template("login_page.html", error=True)
+
+    if not user.check_password(request.form["password"]):
+        return render_template("login_page.html", error=True)
+
+    login_user(user)
+    return redirect(url_for('index'))
+
+
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    #comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"], commenter=current_user)
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.jade',
+        title='Home Page',
+        year=datetime.now().year,
+    )
