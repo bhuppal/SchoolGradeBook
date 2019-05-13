ecd7b73f98accf21d730ba77bb3ea8acc01e5fd2
Bhuppal Kumar <bhuppal@hotmail.com>
2019-05-13 12:09:54 +0000

    Ajax call made to Instructor page

--- mysite/static/js/script.js
+++ mysite/static/js/script.js
@@ -3,10 +3,18 @@ $(document).ready(function() {
 });
 
 
-
-
-
-
+// Instructor Details
+function loadAllInstructorDetails() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("InstructorDetails").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://schoolgradebook.pythonanywhere.com/api/instructor", true);
+  xhttp.send();
+}
 
 
 
