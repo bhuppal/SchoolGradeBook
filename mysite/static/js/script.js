ece2307f1abca60bd046e53d709eac198232fd49
Bhuppal Kumar <bhuppal@hotmail.com>
2019-05-16 01:30:37 +0000

    Completed Instructor page

--- mysite/static/js/script.js
+++ mysite/static/js/script.js
@@ -1,15 +1,11 @@
-$(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-
 // Instructor Details
 function loadAllInstructorDetails() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("InstructorDetails").innerHTML =
-      this.responseText;
+        document.getElementById("InstructorDetails").innerHTML = this.responseText;
+        //var result = JSON.parse(this.responseText);
+        //displayInstructorDetails(this.responseText);
     }
   };
   xhttp.open("GET", "http://schoolgradebook.pythonanywhere.com/api/instructor", true);
@@ -19,11 +15,13 @@ function loadAllInstructorDetails() {
 
 
 
-
-
-
-
-
-
-
+function displayInstructorDetails(arr) {
+  var out = "";
+  var i;
+  for(i = 0; i < arr.length; i++) {
+    out += '<a href="' + arr[i].url + '">' +
+    arr[i].display + '</a><br>';
+  }
+  document.getElementById("InstructorDetails").innerHTML = out;
+}
 
